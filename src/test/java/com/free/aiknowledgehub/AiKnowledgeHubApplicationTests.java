b91d72124794871f35f1007f4323686a40b84792
Liberty-Swine <694522458@qq.com>
2026-04-06 15:59:04 +0800

    feat: 初始化AI Knowledge Hub项目，基于Spring AI Alibaba + RAG + Ollama + ES + MinIO + Redis

---
+++ src/test/java/com/free/aiknowledgehub/AiKnowledgeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.aiknowledgehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiKnowledgeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
